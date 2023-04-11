8a724dcac808eb635a99f4f4a8719cb432ce129b
miranda utami <mirandautami01@student.ciputra.ac.id>
2023-04-11 09:18:50 +0700

    SWIFT TUTORIAL SECTION 5

--- AFL3-0706012110056-MirandaUtami/CircleImage.swift
+++ AFL3-0706012110056-MirandaUtami/CircleImage.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct CircleImage: View {
     var body: some View {
-        Image("img2")
+        Image("moon ga")
         .clipShape(Circle())
         .overlay {
             Circle().stroke(.white, lineWidth: 4)
