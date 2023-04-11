8baa32314ec915cfe9516ef11c7eb3101aba8ef6
miranda utami <mirandautami01@student.ciputra.ac.id>
2023-04-11 08:32:06 +0700

    Initial Commit

---
+++ AFL3-0706012110056-MirandaUtami/AFL3_0706012110056_MirandaUtamiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_0706012110056_MirandaUtamiApp.swift
+//  AFL3-0706012110056-MirandaUtami
+//
+//  Created by miranda on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_0706012110056_MirandaUtamiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AFL3-0706012110056-MirandaUtami/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AFL3-0706012110056-MirandaUtami
+//
+//  Created by miranda on 11/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
