56b1d71fa787bf6ed669def48124b11d0f85f3a2
miranda utami <mirandautami01@student.ciputra.ac.id>
2023-04-11 09:10:48 +0700

    SWIFT TUTORIAL SECTION 4

---
+++ AFL3-0706012110056-MirandaUtami/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  AFL3-0706012110056-MirandaUtami
+//
+//  Created by miranda on 11/04/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("img2")
+        .clipShape(Circle())
+        .overlay {
+            Circle().stroke(.white, lineWidth: 4)
+        }
+        .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
