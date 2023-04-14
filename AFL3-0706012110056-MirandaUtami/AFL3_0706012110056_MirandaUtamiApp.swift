fcefd34a1e6dcb158e0ddca4b0ad29e920ed7c5c
miranda utami <mirandautami01@student.ciputra.ac.id>
2023-04-14 09:38:22 +0700

    Handling User Input Section 5

--- AFL3-0706012110056-MirandaUtami/AFL3_0706012110056_MirandaUtamiApp.swift
+++ AFL3-0706012110056-MirandaUtami/AFL3_0706012110056_MirandaUtamiApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct AFL3_0706012110056_MirandaUtamiApp: App {
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
