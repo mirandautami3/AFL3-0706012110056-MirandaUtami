ff88b55d97500d81bfe96009cea9362ea156d6bd
miranda utami <mirandautami01@student.ciputra.ac.id>
2023-04-20 15:07:21 +0700

    Composing Complex Interfaces Section 2

--- AFL3-0706012110056-MirandaUtami/Model/ModelData.swift
+++ AFL3-0706012110056-MirandaUtami/Model/ModelData.swift
@@ -11,16 +11,21 @@ import Combine
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
-}
 
-var landmarks: [Landmark] = load("landmarkData.json")
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
+}
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
 
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle.")
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
     }
 
     do {
---
+++ AFL3-0706012110056-MirandaUtami/Views/categories/CategoryHome.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryHome.swift
+//  AFL3-0706012110056-MirandaUtami
+//
+//  Created by miranda on 20/04/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
