d963f7fe5dec30e74ec850654797ecd31dda137f
miranda utami <mirandautami01@student.ciputra.ac.id>
2023-04-20 19:42:56 +0700

    Working with UI Controls section 3

--- AFL3-0706012110056-MirandaUtami/Views/Profile/ProfileHost.swift
+++ AFL3-0706012110056-MirandaUtami/Views/Profile/ProfileHost.swift
@@ -8,19 +8,30 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
     @State private var draftProfile = Profile.default
 
     var body: some View {
-    VStack(alignment: .leading, spacing: 20) {
-    ProfileSummary(profile: draftProfile)
+        VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                Spacer()
+                EditButton()
             }
-            .padding()
-        }
-    }
 
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                Text("Profile Editor")
+            }
+        }
+        .padding()
+    }
+}
 
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
