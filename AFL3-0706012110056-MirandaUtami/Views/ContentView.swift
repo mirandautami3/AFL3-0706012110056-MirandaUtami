fef01e7825934aa567b99a0c270fe31d72f39e1d
miranda utami <mirandautami01@student.ciputra.ac.id>
2023-04-20 15:27:46 +0700

    Composing Complex Interfaces section 5

--- AFL3-0706012110056-MirandaUtami/Views/ContentView.swift
+++ AFL3-0706012110056-MirandaUtami/Views/ContentView.swift
@@ -8,40 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack{
-            MapView()
-            .ignoresSafeArea(edges: .top)
-            .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading){
-            Text("Turtle Rock")
-                .font(.title)
-            
-            HStack {
-                Text("Joshua Tree National Park")
-                .font(.subheadline)
-                Spacer()
-                Text("California")
-                .font(.subheadline)
-            }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
+    @State private var selection: Tab = .featured
 
-                Text("About Turtle Rock")
-                .font(.title2)
-                Text("Descriptive text goes here.")
-                Text("")
-        }
-        .padding()
-            
-        Spacer()
+    enum Tab {
+        case featured
+        case list
+    }
+
+    var body: some View {
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
         }
     }
 }
@@ -49,5 +35,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
