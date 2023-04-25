4f4d5bf624b9a4a863ae1a99138d033e2f54815a
miranda utami <mirandautami01@student.ciputra.ac.id>
2023-04-26 00:14:13 +0700

    Interfacing with UIKit section 4

--- AFL3-0706012110056-MirandaUtami/Views/pageview/PageView.swift
+++ AFL3-0706012110056-MirandaUtami/Views/pageview/PageView.swift
@@ -9,15 +9,18 @@ import SwiftUI
 
 struct PageView<Page: View>: View {
     var pages: [Page]
-    @State private var currentPage = 1
+    @State private var currentPage = 0
 
     var body: some View {
-    VStack {
-    PageViewController(pages: pages, currentPage: $currentPage)
-    Text("Current Page: \(currentPage)")
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
         }
     }
 }
+
 struct PageView_Previews: PreviewProvider {
     static var previews: some View {
         PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
